da187565e53511062f558695e33e177bc166e3ed
ericopieri <erico.pieri@gmail.com>
2024-02-17 00:22:57 -0300

    Configurar hosts para deploy no render

--- server.js
+++ server.js
@@ -49,9 +49,13 @@ server.delete("/videos/:id", async (request, reply) => {
     return reply.status(204).send();
 });
 
-server.listen({ port: process.env.PORT ?? 3000 }, (err, address) => {
-    if (err) {
-        console.error(err);
-        exit(1);
-    }
+server.listen({
+        port: process.env.PORT ?? 3000,
+        host: '0.0.0.0'
+    },
+    (err, address) => {
+        if (err) {
+            console.error(err);
+            exit(1);
+        }
 });
